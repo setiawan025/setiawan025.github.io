b068f166d1c0fa26f5eb20b1b8495726a8c08c95
setiawan025 <setiawankrcs@gmail.com>
2024-06-01 15:10:57 +0700

    first commit

---
+++ script/main.js
@@ -0,0 +1,17 @@
+// toggle & responsive navigation
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const navLists = document.querySelector("nav");
+
+  burger.addEventListener("click", () => {
+    navLists.classList.toggle("nav-active");
+    burger.classList.toggle("toggle-burger");
+  });
+};
+navSlide();
+// clear form bifore unload
+window.onbeforeunload = () => {
+  for (const form of document.getElementsByTagName("form")) {
+    form.reset();
+  }
+};
